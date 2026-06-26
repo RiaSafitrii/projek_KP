c350a5088f85660c665d7059caa94936280d8219
Ria Safitri <152170423+RiaSafitrii@users.noreply.github.com>
2026-06-26 10:31:25 +0700

    Create migration for web_option table

---
+++ database/migrations/2024_12_19_221626_create_web_option_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('web_option', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Kolom name
+            $table->text('value'); // Kolom value
+            $table->string('path_file')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('web_option');
+    }
+};
