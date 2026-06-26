d4037e33b4be9fc3bd2bc7791939a1e6b21bb6ee
anindiagitaa <168162278+anindiagitaa@users.noreply.github.com>
2026-06-26 10:42:38 +0700

    Create migration for news table

---
+++ database/migrations/2024_12_19_231105_create_news_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->string('operator');
+            $table->text('content')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->timestamp('publish_date')->nullable();
+            $table->integer('views')->default(0);  // Menyimpan jumlah views
+            $table->integer('shares')->default(0);  // Menyimpan jumlah shares
+            $table->foreignId('category_id')->nullable()->constrained('categories')->onDelete('set null');
+            $table->foreignId('operator_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
