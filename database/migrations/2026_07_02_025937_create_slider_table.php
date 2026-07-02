2a7b20b9fbec82bf1c5baf66d42d111eabcb5d67
RiaSafitrii <ria.safitri2606@gmail.com>
2026-07-02 10:47:34 +0700

    edit again 3

---
+++ database/migrations/2026_07_02_025937_create_slider_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('slider', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->string('button_text')->nullable();
+            $table->string('button_link')->nullable();
+            $table->enum('status', ['actived', 'nonactived'])->default('actived');
+            $table->integer('position')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('slider');
+    }
+};
